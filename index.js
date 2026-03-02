04cb719e1f59e8e5fc1a56bf21b530bf1cd55759
miazi2003 <yeasinmiazi1997@gmail.com>
2026-03-02 23:00:36 +0600

    cors updated

--- index.js
+++ index.js
@@ -10,12 +10,19 @@ import jobRoutes from './routes/jobRoutes.js';
 import applicationRoutes from './routes/applicationRoutes.js';
 
 const app = express();
-
+const corsOrigins = process.env.CORS_ORIGINS.split(",");
 // ===== Middleware =====
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || corsOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 app.use(express.json());
 
 // ===== MongoDB Connection (Mongoose) =====
